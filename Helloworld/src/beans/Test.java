fa8c69d99a0c7ec9435d855e26e86471045a9c70
Ankit Bhatnagar <ankit.bhatnagar1994@gmail.com>
2018-01-31 16:31:25 +0530

    Test

--- Helloworld/src/beans/Test.java
+++ Helloworld/src/beans/Test.java
@@ -1,6 +1,16 @@
 package beans;
+
+
+
 public class Test
 {
+	
+public Test() {
+		
+
+System.out.println("Inside test constructor");
+	}
+
 public void hello()
 {
 System.out.println("hello world");
