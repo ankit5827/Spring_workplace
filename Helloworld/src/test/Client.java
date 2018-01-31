5425902777d4d89cdcc6bacdfae4df052c54809a
Ankit Bhatnagar <ankit.bhatnagar1994@gmail.com>
2018-01-31 17:20:27 +0530

    singleton and prototype

--- Helloworld/src/test/Client.java
+++ Helloworld/src/test/Client.java
@@ -16,7 +16,11 @@ public static void main(String []args)
 Resource r= new ClassPathResource("resources/spring.xml");
 BeanFactory factory=new XmlBeanFactory(r);
 Object o =factory.getBean("t");
+Object o1 =factory.getBean("t");
+Object o2 =factory.getBean("t");
 
+System.out.println(o==o1);//if singleton  it will return true
+System.out.println(o1==o2);//if  prototype it will return false
 Test t=(Test)o;
 t.hello();
 }
