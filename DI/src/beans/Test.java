c6f924fdbb4f7a89cfd8bc3fa83faf385a9257f1
Ankit Bhatnagar <ankit.bhatnagar1994@gmail.com>
2018-02-01 15:58:40 +0530

    DI

---
+++ DI/src/beans/Test.java
@@ -0,0 +1,18 @@
+package beans;
+
+public class Test {
+	public String gender;
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	 public void hello(String name)
+	 {
+		 System.out.println("Hello...."+gender+"....  "+name);
+	 }
+
+}
