314eb842e38c6744df1d5157695a7135aba01cec
Ankit Bhatnagar <ankit.bhatnagar1994@gmail.com>
2018-02-06 17:19:02 +0530

    commit

--- Setter_DI_Required/src/test/Client.java
+++ Setter_DI_Required/src/test/Client.java
@@ -1,12 +1,9 @@
 package test;
 
-import org.springframework.beans.factory.annotation.RequiredAnnotationBeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import beans.Test;
-
-
 
 public class Client {
 
@@ -15,7 +12,7 @@ public class Client {
 		ApplicationContext ap=new ClassPathXmlApplicationContext("resources/spring.xml");
 		Test t=(Test) ap.getBean("t");
 		t.printConn();
-		RequiredAnnotationBeanPostProcessor
+		
 		
 	}
 
