948035cdf95a9889aedb28c80e943257306670d1
Ankit Bhatnagar <ankit.bhatnagar1994@gmail.com>
2018-02-09 15:54:34 +0530

    FactoryClasses

---
+++ FactoryClasses/src/beans/CarFactory.java
@@ -0,0 +1,18 @@
+package beans;
+
+
+
+public class CarFactory {
+	
+	public static String carname;
+	public static void setCarname(String carname) {
+		CarFactory.carname = carname;
+	}
+	//static factorty approach 
+	public static Car  getInstance () throws Exception
+	{
+		Car c=(Car)Class.forName(carname).newInstance();
+		return c;
+	}
+
+}
